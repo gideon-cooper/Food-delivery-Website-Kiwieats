db05bdc0e00b4cb67988e7918fbd9da60e949aed
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-10-11 12:39:23 +1300

    image added and button not that responsive so will need to add that in the future

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,33 @@ const useStyles = makeStyles((theme) => ({
   home: {
     display: 'flex',
     minHeight: '80vh',
+    width: '90%',
     alignItems: 'center',
+    ['@media (max-width:1024px)']: {
+      flexDirection: 'column',
+    },
+  },
+  title: {
+    color: 'green',
+  },
+  button: {
+    backgroundColor: 'green',
+    marginTop: '20px',
+    color: 'whitesmoke',
+    '&:hover': {
+      backgroundColor: '#3FC060',
+    },
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
+  left: {
+    flex: 1,
+  },
+  right: {
+    flex: 2,
+    height: '70vh',
   },
 }))
 
@@ -16,12 +42,22 @@ export default function Home() {
   const classes = useStyles()
   return (
     <Container className={classes.home}>
-      <div className={classes.color1}>
-        <Typography>Get Food</Typography>
-        <Typography>Delivered now</Typography>
-        <Button color="primary">Order now</Button>
+      <div className={classes.left}>
+        <Typography variant="h2">Get Food</Typography>
+        <Typography className={classes.title} variant="h2">
+          Delivered now
+        </Typography>
+        <Button className={classes.button} variant="contained">
+          Order now
+        </Button>
       </div>
-      <div className={classes.color2}></div>
+      <div className={classes.right}>
+        <img
+          className={classes.image}
+          src="https://img.deszone.net/2018/10/food-vector-free-icon-set2.jpg"
+          alt=""
+        />
+      </div>
     </Container>
   )
 }
